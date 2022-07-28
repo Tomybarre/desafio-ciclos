241b3e95033996c3fa4cb8e38f6c05d754ec9524
tomybarre <tomybarre@gmail.com>
2022-07-28 17:22:17 -0300

    ciclos desafio

---
+++ js/ciclo.js
@@ -0,0 +1,23 @@
+/*for(let i = 1; i <=15; i++){
+    console.log("iteracion ${i}");
+}
+console.log('ciclo final');*/
+
+/*while*/
+
+/*let x = 1;
+while (x <= 15){
+    console.log('iteracion ${x}');
+    x++;
+}
+console.log('fin')*/
+
+/*do while*/
+
+let y = 1;
+
+do{
+    console.log('iteracion ${y}');
+    y++;
+
+} while (y <= 15);
